d17295af6b43f2f05b555353f255138dbb09315d
leonkozlowski <leonkozlowski@gmail.com>
2019-09-16 22:09:31 -0400

    ignore some ide stuff + cl constants

---
+++ src/constants.py
@@ -0,0 +1,37 @@
+import os
+
+##############
+# CRAIGSLIST #
+##############
+
+CL_BASE = 'craigslist.org'
+
+CL_BASE_SEARCH = '/search/sss?query='
+CL_BASE_TAIL = 'sort=rel'
+
+CL_CITIES = [
+    'atlanta',
+    'austin',
+    'boston',
+    'chicago',
+    'dallas',
+    'denver',
+    'detroit',
+    'global',
+    'houston',
+    'lasvegas',
+    'losangeles',
+    'miami',
+    'minneapolis',
+    'newyork',
+    'orangecounty',
+    'philadelphia',
+    'phoenix',
+    'portland',
+    'raleigh',
+    'sacramento',
+    'sandiego',
+    'seattle',
+    'sfbay',
+    'washingtondc'
+]
