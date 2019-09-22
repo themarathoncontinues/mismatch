1c84067dc5df2d3edbe4b1f24b86e8267177bf46
Mitchell Bregman <mitchbregs@gmail.com>
2019-09-21 23:55:19 -0400

    added to url util instead of standalone

--- src/utils/url_util.py
+++ src/utils/url_util.py
@@ -1,4 +1,5 @@
 import re
+import requests
 
 
 def cl_product(product_arg):
@@ -11,3 +12,19 @@ def cl_product(product_arg):
     cl_url_product = '+'.join(str(w) for w in word_list)
 
     return cl_url_product
+
+
+def shorten(url):
+
+    endpoint = "http://tinyurl.com/api-create.php"
+
+    params = {
+    	'url': url
+    }
+
+    try:
+        res = requests.get(endpoint, params=params)
+    except Exception as e:
+        raise
+
+    return res.text
